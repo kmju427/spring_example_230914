b4becea6f8996715b1db06a0db6b3c142d186d36
kimminju <test@test.com>
2023-12-15 18:44:06 +0900

    12-15 2강 끝, 3강 시작

--- src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
+++ src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
@@ -2,17 +2,22 @@ package com.example.lesson02.bo;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.lesson02.domain.UsedGoods;
+import com.example.lesson02.mapper.UsedGoodsMapper;
 
 @Service // spring bean
 public class UsedGoodsBO {
 
+	@Autowired // Dependency Injecttion(DI) : 의존성 주입
+	private UsedGoodsMapper usedGoodsMapper; // Spring bean을 가져온다.
+	
 	// input : X (Controller한테 받아온다.)
 	// output : (Repository한테 받은 것을) List<UsedGoods> (Controller한테 준다.)
 	public List<UsedGoods> getUsedGoodsList() {
-		return ;
+		return usedGoodsMapper.selectUsedGoodsList();
 	}
 	
 } // public class UsedGoodsBO
