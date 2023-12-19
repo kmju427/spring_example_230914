c7e6bfb994be252bb602674fc106c8a962528523
kimminju <test@test.com>
2023-12-19 18:42:38 +0900

    12-19 수업 내용

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -18,4 +18,24 @@ public class ReviewBO {
 		return reviewMapper.selectReview(id);
 	}
 	
+	// input : Review
+	// output : X or 성공한 행의 개수(int)
+	public int addReview(Review review) {
+		return reviewMapper.insertReview(review);
+	}
+	
+	// input : addReviewAsField(4, "콤비네이션R", "김바다", 5.0, "역시 맛있다!")
+	// output : X or 성공한 행의 개수(int)
+	// BO와 Mapper의 변수명은 아무 상관 없다. 서로 달라도 아무런 문제되지 않는다.
+	// 컨트롤러에서 넘어오는 변수의 순서는 제대로 맞춰야 한다.
+	public int addReviewAsField(int storeId, String menu, String userName, Double point, String review) {
+		return reviewMapper.insertReviewAsField(storeId, menu, userName, point, review);
+	}
+	
+	// input : id, review
+	// output : 성공한 행의 개수
+	public int updateReviewById(int id, String review) {
+		return reviewMapper.updateReviewById(id, review);
+	}
+	
 } // public class ReviewBO
--- src/main/java/com/example/lesson03/mapper/ReviewMapper.java
+++ src/main/java/com/example/lesson03/mapper/ReviewMapper.java
@@ -1,5 +1,6 @@
 package com.example.lesson03.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.lesson03.domain.Review;
@@ -10,5 +11,26 @@ public interface ReviewMapper {
 	// input : id
 	// output : Review
 	public Review selectReview(int id);
+	
+	// input : Review
+	// output : X or 성공한 행의 개수(int)
+	public int insertReview(Review review);
+	
+	// input : addReviewAsField(4, "콤비네이션R", "김바다", 5.0, "역시 맛있다!")
+	// output : X or 성공한 행의 개수(int)
+	public int insertReviewAsField(
+			// 하나의 map이 된 것 -> @Param
+			// .xml에서는 변수명이 아닌 키명으로 매핑된다.
+			@Param("storeId") int storeId, 
+			@Param("menu") String menu, 
+			@Param("userName") String userName, 
+			@Param("point") Double point, 
+			@Param("review") String review);
+	
+	// input : id, review
+	// output : 성공한 행의 개수
+	public int updateReviewById(
+			@Param("id") int id, 
+			@Param("review") String review);
 	
 } // public interface ReviewMapper
